75193513d472099f681f194c703ff2dff087e127
Daniel O. Delgado <danielo.delgado@gmail.com>
2025-08-06 16:31:18 -0600

    adds requireLogin decorator

--- src/command_handler.ts
+++ src/command_handler.ts
@@ -3,9 +3,32 @@ import * as userQueries from './lib/db/queries/users';
 import * as feedQueries from './lib/db/queries/feeds';
 import * as feedFollowQueries from './lib/db/queries/feed_follows';
 import { fetchFeed } from './rss';
+import type { SelectUser } from './lib/db/schema';
+
 
 type CommandHandler = (cmdName: string, ...args: string[]) => Promise<void>;
 
+type UserCommandHandler = (
+  cmdName: string,
+  user: SelectUser,
+  ...args: string[]
+) => Promise<void>;
+
+
+export function requireLogin(handler: UserCommandHandler): CommandHandler {
+    return async (cmdName: string, ...args: string[]) => {
+        const currentUser = getUser();
+        if (!currentUser) {
+            throw new Error("No user logged in");
+        }
+
+        const user = await userQueries.getUserByName(currentUser);
+        if (!user) {
+            throw new Error(`User ${currentUser} does not exist`);
+        }
+        await handler(cmdName, user, ...args);
+    };
+}
 
 export async function handlerLogin(cmdName: string, ...args: string[]) {
     if (args.length == 0 || args[0] === undefined){
@@ -54,22 +77,14 @@ export async function handlerAggregate(cmdName: string, ...args: string[]) {
     console.log(JSON.stringify(feed, null, 2));
 }
 
-export async function handlderAddFeed(cmdName: string, ...args: string[]) {
+export async function handlderAddFeed(cmdName: string, user: SelectUser, ...args: string[]) {
     if (args.length < 2 || args[0] === undefined || args[1] === undefined){
         throw new Error("Feed name and URL are required");
     }
     const name = args[0];
     const url = args[1];
-    const currentUser = getUser();
-    if (!currentUser) {
-        throw new Error("No user logged in");
-    }
-    const user = await userQueries.getUserByName(currentUser);
-    if (!user) {
-        throw new Error(`User ${currentUser} does not exist`);
-    }
     const feed = await feedQueries.createFeed(name, url, user.id);
-    console.log(`Feed ${name} added successfully for user ${currentUser}`);
+    console.log(`Feed ${name} added successfully for user ${user.name}`);
 
     await feedFollowQueries.createFeedFollow(user.id, feed.id);
 }
@@ -82,7 +97,7 @@ export async function handlerFeeds(cmdName: string, ...args: string[]) {
     }
 }
 
-export async function handlerFollow(cmdName: string, ...args: string[]) {
+export async function handlerFollow(cmdName: string, user: SelectUser,...args: string[]) {
     if (args.length == 0 || args[0] === undefined){
         throw new Error("Url is required");
     }
@@ -91,38 +106,18 @@ export async function handlerFollow(cmdName: string, ...args: string[]) {
         throw new Error(`Feed ${args[0]} does not exist`);
     }
 
-    const currentUser = getUser();
-    if (!currentUser) {
-        throw new Error("No user logged in");
-    }
-
-    const user = await userQueries.getUserByName(currentUser);
-    if (!user) {
-        throw new Error(`User ${currentUser} does not exist`);
-    }
-
     await feedFollowQueries.createFeedFollow(user.id, feed.id);
-    console.log(`Feed "${feed.name}" followed successfully by user ${currentUser}`);
+    console.log(`Feed "${feed.name}" followed successfully by user ${user.name}`);
 }
 
-export async function handlerFollowing(cmdName: string, ...args: string[]) {
-    const currentUser = getUser();
-    if (!currentUser) {
-        throw new Error("No user logged in");
-    }
-
-    const user = await userQueries.getUserByName(currentUser);
-    if (!user) {
-        throw new Error(`User ${currentUser} does not exist`);
-    }
-
+export async function handlerFollowing(cmdName: string, user: SelectUser, ...args: string[]) {
     const follows = await feedFollowQueries.getFeedFollowsForUser(user.id);
     if (follows.length === 0) {
-        console.log(`${currentUser} is not following any feeds.`);
+        console.log(`${user.name} is not following any feeds.`);
         return;
     }
 
-    console.log(`${currentUser} is following the following feeds:`);
+    console.log(`${user.name} is following the following feeds:`);
     for (const follow of follows) {
         console.log(`- ${follow.feed}`);
     }
--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,10 @@ async function main() {
     cmd.registerCommand(cmdRegistry, 'reset', cmd.handlerReset);
     cmd.registerCommand(cmdRegistry, 'users', cmd.handlerUsers);
     cmd.registerCommand(cmdRegistry, 'agg', cmd.handlerAggregate);
-    cmd.registerCommand(cmdRegistry, 'addfeed', cmd.handlderAddFeed);
+    cmd.registerCommand(cmdRegistry, 'addfeed', cmd.requireLogin(cmd.handlderAddFeed));
     cmd.registerCommand(cmdRegistry, 'feeds', cmd.handlerFeeds);
-    cmd.registerCommand(cmdRegistry, 'follow', cmd.handlerFollow);
-    cmd.registerCommand(cmdRegistry, 'following', cmd.handlerFollowing);
+    cmd.registerCommand(cmdRegistry, 'follow', cmd.requireLogin(cmd.handlerFollow));
+    cmd.registerCommand(cmdRegistry, 'following', cmd.requireLogin(cmd.handlerFollowing));
 
     try {
         await cmd.runCommand(cmdRegistry, args[0], ...args.slice(1));
